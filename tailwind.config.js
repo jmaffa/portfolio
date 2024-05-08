8c31f9a0b0e1d1ff2fb30ce2be7a18ce2d71cdfe
jmaffa <90526195+jmaffa@users.noreply.github.com>
2024-05-07 22:40:10 -0400

    homepage mostly done, may add links about eating, photos, and music. and also the photo is a little sus but don't look

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,8 @@ const config: Config = {
     },
     colors: {
       'jm-blue' : '#383D6B',
-      'jm-yellow' : '#DFC361'
+      'jm-yellow' : '#DFC361',
+      'jm-red' : '#F24822'
     }
   },
   plugins: [],
